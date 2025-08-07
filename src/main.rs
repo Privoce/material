d4bfb7d51c47bc67c328c69e6afd2e482313ff3a
syf20020816 <syf20020816@outlook.com>
2025-08-07 21:54:52 +0800

    pub all

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     tracing_subscriber::fmt().init();
 
     // Bind server to port 5800
-    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    let acceptor = TcpListener::new("0.0.0.0:5800").bind().await;
 
     // Start serving requests
     Server::new(acceptor).serve(router::build()).await;
